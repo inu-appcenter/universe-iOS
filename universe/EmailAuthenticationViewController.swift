8b2d6580b5e6a18ff361c254353b0a7200a75ecf
eungu <eungu0920@naver.com>
2021-07-26 21:07:51 +0900

    로그인, 회원가입, 이메일 인증 화면 구성

---
+++ universe/EmailAuthenticationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EmailAuthenticationViewController.swift
+//  universe
+//
+//  Created by 은구 on 2021/07/26.
+//
+
+import UIKit
+
+class EmailAuthenticationViewController: UIViewController {
+    // 이메일 인증과 시간내 인증번호 입력 구현은 어떻게?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ universe/SignUpViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SignUpViewController.swift
+//  universe
+//
+//  Created by 은구 on 2021/07/26.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    // 각 사용자의 이메일과 패스워드 저장은 어떻게?
+    
+    /*
+     MVVM Design Pattern
+     
+     Model
+     -
+     
+     
+     View
+     -
+     
+     
+     ViewModel
+     -
+     */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
